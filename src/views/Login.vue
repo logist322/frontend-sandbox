ba0c116919893c9881bc9d4521a9ae1c4bd02db5
logist322 <53333039+logist322@users.noreply.github.com>
2021-08-30 14:13:40 +0300

    Create UI
    - create Login view
    - create Room view

---
+++ src/views/Login.vue
@@ -0,0 +1,11 @@
+<template>
+  <login-form />
+</template>
+
+<script>
+import LoginForm from "@/components/LoginForm.vue";
+export default {
+  components: { LoginForm },
+  name: "Login",
+};
+</script>
---
+++ src/views/Room.vue
@@ -0,0 +1,11 @@
+<template>
+  <room-interface />
+</template>
+
+<script lang="ts">
+import RoomInterface from "../components/RoomInterface.vue";
+
+export default {
+  components: { RoomInterface },
+};
+</script>
