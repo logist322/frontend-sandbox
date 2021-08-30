168cc244ea5966063534c85c9b7f478c926c24bd
logist322 <53333039+logist322@users.noreply.github.com>
2021-08-30 11:15:10 +0000

    Deploying to gh-pages from @ logist322/frontend-sandbox@ba0c116919893c9881bc9d4521a9ae1c4bd02db5 🚀

--- js/about.576cb3c9.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkfrontend_sandbox"]=self["webpackChunkfrontend_sandbox"]||[]).push([[443],{380:function(n,e,t){t.r(e),t.d(e,{default:function(){return c}});var s=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},u=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"about"},[t("h1",[n._v("This is an about page")])])}],a=t(1),r={},l=(0,a.Z)(r,s,u,!1,null,null,null),c=l.exports}}]);
-//# sourceMappingURL=about.576cb3c9.js.map
