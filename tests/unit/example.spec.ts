48c76745870ffa6e45909879a48d998777cac07a
silentPartner <hytornoy-99@mail.ru>
2021-09-02 14:51:30 +0300

    Configure mocha and karma for testing

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,10 @@
+import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import LoginForm from "@/components/LoginForm.vue";
 
-describe("HelloWorld.vue", () => {
+describe("LoginForm.vue", () => {
   it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
+    const wrapper = shallowMount(LoginForm);
+    expect(wrapper.exists()).equal(true);
   });
 });
